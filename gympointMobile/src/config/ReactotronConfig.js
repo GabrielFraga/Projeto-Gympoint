fdc0d8b6af69190a1db3dd1cacf628bdaa335a49
GabrielFraga <gabrielgoog@gmail.com>
2020-01-03 00:47:50 -0300

    variaveis de ambiente

--- gympointMobile/src/config/ReactotronConfig.js
+++ gympointMobile/src/config/ReactotronConfig.js
@@ -3,9 +3,11 @@ import { reactotronRedux } from 'reactotron-redux';
 import sagaPlugin from 'reactotron-redux-saga';
 import AsyncStorage from '@react-native-community/async-storage';
 
+import { BASE_URL } from 'react-native-dotenv';
+
 if (__DEV__) {
   const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
-    .configure({ host: '192.168.1.12' })
+    .configure({ host: BASE_URL })
     .useReactNative()
     .use(reactotronRedux())
     .use(sagaPlugin())
